f1079f0ca2d9a5a4608752eaa950c6690e0efdd5
lawrence <knightboy500@gmail.com>
2024-07-27 17:44:47 +0100

    adds: functinal sketch box

--- script.js
+++ script.js
@@ -1,29 +1,52 @@
-const settingsBtn = document.getElementById('settings-btn')
-const settings = document.querySelector('.settings-box')
+const settingsBtn = document.getElementById("settings-btn");
+const settings = document.querySelector(".settings-box");
 
-settingsBtn.addEventListener('click', () => {
-    settings.classList.toggle('open')
-})
+settingsBtn.addEventListener("click", () => {
+  settings.classList.toggle("open");
+});
+
+let sketchAreaSize = 850;
+let sketchAreaRows = 100;
+let sketchAreaCols = 100;
+
+let brushActive = false;
+
+const sketchArea = document.querySelector(".sketch-area");
+sketchArea.style.width = `${sketchAreaSize}px`;
+sketchArea.style.height = `${sketchAreaSize}px`;
 
 
-let sketchAreaSize = 700
-let sketchAreaRows = 100
-let sketchAreaCols = 100
 
-const sketchArea = document.querySelector('.sketch-area')
-sketchArea.style.width = `${sketchAreaSize}px`
-sketchArea.style.height = `${(sketchAreaSize)}px`
+document.body.addEventListener("keydown", (e) => {
+  console.log(e.code);
+  if (e.code === "ShiftLeft") {
+    brushActive = true;
+  }
+});
 
+document.body.addEventListener("keyup", (e) => {
+  if (e.code === "ShiftLeft") {
+    brushActive = false;
+  }
+});
 
-function createSketchArea(){
-    const rows = sketchAreaRows
-    const cols  = sketchAreaCols
-    for(let  i = 0; i < (rows * cols); i++){
-        const sketchGridBox = document.createElement('div')
-        sketchGridBox.style.width = `${(sketchAreaSize / rows)}px`
-        sketchGridBox.style.height = `${(sketchAreaSize / cols)}px`
-        document.querySelector('.sketch-area').append(sketchGridBox)
-    }
+function handleBoxChange(element) {
+  if (brushActive) element.style.backgroundColor = "yellow";
 }
 
-createSketchArea()
+function createSketchArea() {
+  const rows = sketchAreaRows;
+  const cols = sketchAreaCols;
+  for (let i = 0; i < rows * cols; i++) {
+    const sketchGridBox = document.createElement("div");
+    sketchGridBox.style.width = `${sketchAreaSize / rows}px`;
+    sketchGridBox.style.height = `${sketchAreaSize / cols}px`;
+    document.querySelector(".sketch-area").append(sketchGridBox);
+
+    sketchGridBox.addEventListener("mouseover", () =>
+      handleBoxChange(sketchGridBox)
+    );
+  }
+}
+
+createSketchArea();
